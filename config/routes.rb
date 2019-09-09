3821b38a437303b883adcbfdbcc8b690f077fa06
michinoku61 <michinoku61@gmail.com>
2019-09-09 15:02:26 +0900

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root 'products#index'
-  get 'products#show'
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
   get  'items'      =>  'items#index'
   get  'items/show'      =>  'items#show'
   get  'items/buycheck'      =>  'items#buycheck'
   get  'items/buy'      =>  'items#buy'
 
->>>>>>> master
 end

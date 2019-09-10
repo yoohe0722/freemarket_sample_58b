ce66c4e8534a16eceb15953da3201630acab2a83
michinoku61 <michinoku61@gmail.com>
2019-09-10 16:42:44 +0900

    ルーティング訂正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
-  get  'items'      =>  'items#index'
-  get  'items/show'      =>  'items#show'
-  get  'items/buycheck'      =>  'items#buycheck'
-  get  'items/buy'      =>  'items#buy'
-
+  resources :items, only: [:index, :show] do
+    collection do
+      get 'buy'
+      get 'buycheck'
+    end
+  end
 end

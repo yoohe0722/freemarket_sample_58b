e87b654701b71def9ef580bc1f5632aee56a9c33
Yohei Higashi <yoohe0722@gmail.com>
2019-09-15 15:27:07 +0900

    brandsテーブルにgroup_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_055646) do
+ActiveRecord::Schema.define(version: 2019_09_15_061553) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_09_15_055646) do
     t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "group_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

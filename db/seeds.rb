bbc7af9170ef635bddcdba53dd9729da48300b63
Yohei Higashi <yoohe0722@gmail.com>
2019-09-15 14:19:15 +0900

    seeds.rbの記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+# brand
+CSV.foreach('db/brands.csv') do |row|
+  Brand.create(name: row[0], group_id: row[1])
+end
+
+#brand_group
+CSV.foreach('db/brand_group.csv') do |row|
+  Brand_group.create(name: row[0])
+end
+
+#size
+CSV.foreach('db/size.csv') do |row|
+  Size.create(name: row[0], gourp_id: row[1])
+end
+
+#size_group(保留)
+# CSV.foreach('db/size_groups.csv') do |row|
+#   Size_group.create(name: row[0])
+# end
+
+# 別の書き方
+# brands_csv = CSV.readlines("db/brands.csv")
+# brands_csv.shift
+# brands_csv.each do |row|
+#   Brand.create(name: row[1])
+# end

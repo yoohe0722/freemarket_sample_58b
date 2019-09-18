62c3aec625dd3ed8db365d880ca178eecf3fd3c3
Yohei Higashi <yoohe0722@gmail.com>
2019-09-18 13:31:12 +0900

    ファクトリーボット導入

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
+
     it "is invalid without a family_name" do
-      user = User.new(family_name: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, family_name: nil)
       user.valid?
       expect(user.errors[:family_name]).to include("can't be blank")
     end
+
+    it "is invalid without a email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
   end
 end

9733818cc54e97df91b9a9647a9cefd923e2f385
Apple-cake <tongensai.kudasai@gmail.com>
2019-09-18 13:39:00 +0900

    credentials.ymlの記述を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :show] do
+  resources :items, only: [:index, :show, :create] do
     collection do
       get 'buy'
       get 'buycheck'
@@ -11,6 +11,16 @@ Rails.application.routes.draw do
       get 'logout'
       get 'user_edit'
       get 'identification'
+      get 'signup_registration'
+      post 'items' => 'items#create'
+    end
+  end
+  resources :users, only: [:index, :show, :new, :edit] do
+    collection do
+      get 'credit'
+      get 'complete'
+      get 'phone_authentication'
+      get 'address_input'
     end
   end
 end

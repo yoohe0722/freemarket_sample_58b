88ab38031e3a56795ba55cf31b94796ec9a1d12c
michinoku61 <michinoku61@gmail.com>
2019-09-18 13:19:03 +0900

    コントローラー（createアクション）実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many_attached :images
   belongs_to :category
-  belongs_to :size
-  belongs_to :brand
+  belongs_to :size, optional: true
+  belongs_to :brand, optional: true
 end

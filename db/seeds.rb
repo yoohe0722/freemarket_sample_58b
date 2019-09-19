b68272c014aff967dfd4b63f10986ad3e3b5336c
Yohei Higashi <yoohe0722@gmail.com>
2019-09-19 16:11:28 +0900

    seedの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,42 @@
 require "csv"
 
-# # brand
-# CSV.foreach('db/brands.csv') do |row|
-#   Brand.create(name: row[0], brand_group_id: row[1])
-# end
+# brand
+CSV.foreach('db/brands.csv') do |row|
+  Brand.create(name: row[0], brand_group_id: row[1])
+end
 
-# # #brand_group
-# CSV.foreach('db/brand_group.csv') do |row|
-#   BrandGroup.create(name: row[0])
-# end
+# brand_group
+CSV.foreach('db/brand_group.csv') do |row|
+  BrandGroup.create(name: row[0])
+end
 
-# # #size
-# CSV.foreach('db/size.csv') do |row|
-#   Size.create(name: row[0])
-# end
+# size
+CSV.foreach('db/size.csv') do |row|
+  Size.create(name: row[0])
+end
 
 # user
-# user = User.new(
-#   email: 'komi@canon',
-#   encrypted_password: '12345678',
-#   password: '12345678',
-#   nick_name: 'komican',
-#   family_name: 'test',
-#   family_kana: 'てすと',
-#   first_name: 'taro',
-#   first_kana: 'たろう',
-#   birth_year: '2000',
-#   birth_month: '1',
-#   birth_day: '1',
-#   phone_number: '01234567890',
-#   zip: '1234567',
-#   prefecture_id: '27',
-#   city: '大阪市中央区',
-#   block: '○○町',
-#   building: '1-2-3',
-#   user_id: '1',
-# )
-# user.save!
+user = User.new(
+  email: 'komi@canon',
+  encrypted_password: '12345678',
+  password: '12345678',
+  nick_name: 'komican',
+  family_name: 'test',
+  family_kana: 'てすと',
+  first_name: 'taro',
+  first_kana: 'たろう',
+  birth_year: '2000',
+  birth_month: '1',
+  birth_day: '1',
+  phone_number: '01234567890',
+  zip: '1234567',
+  prefecture_id: '27',
+  city: '大阪市中央区',
+  block: '○○町',
+  building: '1-2-3',
+  user_id: '1',
+)
+user.save!
 
 user = User.new(
   email: 'hanma@baki',

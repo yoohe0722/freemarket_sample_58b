c4458ec447513f25f9b40877716caefb4a536f59
Yohei Higashi <yoohe0722@gmail.com>
2019-09-21 15:02:04 +0900

    signupコントローラーの作成

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,48 @@
 class SignupController < ApplicationController
+
+  def step1
+    @user = User.new # 新規インスタンス作成
+  end
+
+  def step2
+    # step1で入力された値をsessionに保存
+    session[:nick_name] = user_params[:nick_name]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    @user = User.new # 新規インスタンス作成
+  end
+
+  def step3
+    # step2で入力された値をsessionに保存
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new # 新規インスタンス作成
+  end
+
+  def step4
+    # step3で入力された値をsessionに保存
+    session[:family_name] = user_params[:family_name]
+    session[:family_kana] = user_params[:family_kana]
+    session[:first_name] = user_params[:first_name]
+    session[:first_kana] = user_params[:first_kana]
+    session[:zip] = user_params[:zip]
+    session[:prefecture_id] = user_params[:prefecture_id]
+    session[:city] = user_params[:city]
+    session[:block] = user_params[:block]
+    session[:building] = user_params[:building]
+    @user = User.new # 新規インスタンス作成
+  end
+
+
+  def complete
+    # step2で入力された値をsessionに保存
+    session[:family_name] = user_params[:family_name]
+    session[:family_kana] = user_params[:family_kana]
+    session[:first_name] = user_params[:first_name]
+    session[:first_kana] = user_params[:first_kana]
+    @user = User.new # 新規インスタンス作成
+  end
+
   private
     # 許可するキーを設定します
     def user_params
@@ -7,23 +51,17 @@ class SignupController < ApplicationController
         :password,
         :password_confirmation,
         :nick_name,
-        :family_name
-        :family_kana
+        :family_name,
+        :family_kana,
         :first_name,
         :first_kana,
-        :birth_year,
-        :birth_month,
-        :birth_day,
-        :phone_number
-        :zip
-        :prefecture_i
+        :phone_number,
+        :zip,
+        :prefecture_id,
         :city,
         :block,
         :building,
-        
-        :last_name_kana,
-        
-        ~省略~
+        :payment_id,
     )
     end
 end

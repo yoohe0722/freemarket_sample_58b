f660fc612228d62680095a2472dd0b0f3f09a3f9
Yohei Higashi <yoohe0722@gmail.com>
2019-09-21 14:33:17 +0900

    signup_controllerの修正

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,29 @@
 class SignupController < ApplicationController
+  private
+    # 許可するキーを設定します
+    def user_params
+      params.require(:user).permit(
+        :email,
+        :password,
+        :password_confirmation,
+        :nick_name,
+        :family_name
+        :family_kana
+        :first_name,
+        :first_kana,
+        :birth_year,
+        :birth_month,
+        :birth_day,
+        :phone_number
+        :zip
+        :prefecture_i
+        :city,
+        :block,
+        :building,
+        
+        :last_name_kana,
+        
+        ~省略~
+    )
+    end
 end

2b95eba8b80ac95b59439507f0002f0e143776ff
Yohei Higashi <yoohe0722@gmail.com>
2019-09-21 21:40:12 +0900

    signup_controllerの記述

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -10,6 +10,10 @@ class SignupController < ApplicationController
     session[:email] = user_params[:email]
     session[:password] = user_params[:password]
     session[:password_confirmation] = user_params[:password_confirmation]
+    session[:birth_year] = user_params[:birth_year]
+    session[:birth_month] = user_params[:birth_month]
+    session[:birth_day] = user_params[:birth_day]
+
     @user = User.new # 新規インスタンス作成
   end
 
@@ -35,12 +39,38 @@ class SignupController < ApplicationController
 
 
   def complete
-    # step2で入力された値をsessionに保存
-    session[:family_name] = user_params[:family_name]
-    session[:family_kana] = user_params[:family_kana]
-    session[:first_name] = user_params[:first_name]
-    session[:first_kana] = user_params[:first_kana]
+    session[:payment_id] = user_params[:payment_id]
     @user = User.new # 新規インスタンス作成
+  end
+
+  def create
+    @user = User.new(
+      nick_name: session[:nick_name], # sessionに保存された値をインスタンスに渡す
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      family_name: session[:family_name],
+      first_name: session[:first_name],
+      family_kana: session[:family_kana],
+      first_kana: session[:first_kana],
+      phone_number: session[:phone_number],
+      birth_year: session[:birth_year],
+      birth_month: session[:birth_month],
+      birth_day: session[:birth_day],
+      zip: session[:zip],
+      prefecture_id: session[:prefecture_id],
+      city: session[:city],
+      block: session[:block],
+      building: session[:building],
+      payment_id: session[:payment_id],
+    )
+    # binding.pry
+    if @user.save
+      session[:id] = @user.id
+      redirect_to complete_signup_index_path
+    else
+      render '/signup/step1'
+    end
   end
 
   private
@@ -56,6 +86,9 @@ class SignupController < ApplicationController
         :first_name,
         :first_kana,
         :phone_number,
+        :birth_year,
+        :birth_month,
+        :birth_day,
         :zip,
         :prefecture_id,
         :city,

c90c9ad646cae1a7295949a4cd723e8082696b23
michinoku61 <michinoku61@gmail.com>
2019-09-23 11:24:34 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_09_20_100001) do
 
   create_table "brand_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
+    t.integer "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -107,6 +108,7 @@ ActiveRecord::Schema.define(version: 2019_09_20_100001) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string "nick_name"
     t.string "family_name"
     t.string "first_name"
     t.string "family_kana"
@@ -114,7 +116,6 @@ ActiveRecord::Schema.define(version: 2019_09_20_100001) do
     t.integer "birth_year"
     t.integer "birth_month"
     t.integer "birth_day"
-    t.string "nick_name"
     t.string "phone_number"
     t.integer "zip"
     t.integer "prefecture_id"

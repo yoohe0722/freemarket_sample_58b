1419140b859dd4b625b1a64147fbdb50035f58ff
michinoku61 <michinoku61@gmail.com>
2019-09-24 12:47:17 +0900

    destroyメソッド

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :login_check, only: [:buy, :shipping]
-  before_action :set_item, only: [:show, :show_edit_delete]
+  before_action :set_item, only: [:show, :show_edit_delete, :destroy]
 
   def create
     @item = Item.create(item_params)
@@ -57,9 +57,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.user_id == current_user.id
-      item.destroy
+    if @item.user_id == current_user.id
+      @item.destroy
       redirect_to root_path, notice: '商品を削除しました'
     else
       redirect_to root_path, alert: 'ログインユーザーでないため、商品の削除に失敗しました。'

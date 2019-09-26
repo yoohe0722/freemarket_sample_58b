321fc5def1c59360e7e6446589d4b255df104e35
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-09-26 13:20:07 +0900

    binding.pryを削除

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -64,7 +64,6 @@ class SignupController < ApplicationController
       building: session[:building],
       payment_id: session[:payment_id],
     )
-    binding.pry
     if @user.save
       session[:id] = @user.id
       redirect_to complete_signup_index_path

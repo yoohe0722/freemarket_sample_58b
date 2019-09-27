d5d54f9833312cfe77fcb7a967e758293400c252
Yohei Higashi <yoohe0722@gmail.com>
2019-09-27 16:11:06 +0900

    users_controllerに#edit#updateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
   def new
   end
 
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.id == current_user.id
+      @user.update(user_update_params)
+      redirect_to user_path, notice: 'ユーザー情報を更新しました'
+    else
+      render user_path, notice: 'ユーザー情報の更新に失敗しました'
+    end
+
+  end
 
   def credit
   end
@@ -21,5 +34,9 @@ class UsersController < ApplicationController
     @item = user.items.all
   end
 
-
+  private
+  def user_update_params
+    # binding.pry
+    params.fetch(:user,{}).permit(:nick_name, :image, :profile).merge(current_user.id)
+  end
 end

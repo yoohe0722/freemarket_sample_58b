91dd19939cefddd71fe2c2255586d05873d77f71
Yohei Higashi <yoohe0722@gmail.com>
2019-09-30 21:45:11 +0900

    carousel.jsの編集

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,10 +1,11 @@
-$(document).on('turbolinks:load', function(){
-  
+$(function(){
   $('.slider').slick({
     autoplay:true,
-    autoplaySpeed: 2000,
-    speed: 100000,
+    autoplaySpeed: 3000,
+    speed: 1500,
     dots:true,
+    // slickArrow: '<img src="https://web-jp-assets.mercdn.net/_next/static/images/carousel-prev-af7ca886ce42c4c02610cd15421ddcad.png" class="slide-arrow prev-arrow slick-prev">',
+    // nextArrow: '<img src="https://web-jp-assets.mercdn.net/_next/static/images/carousel-next-bc3a1438a1435e953bc67d720cd05c61.png" class="slide-arrow next-arrow slick-next">'
   });
-  console.log(this);
 });
+

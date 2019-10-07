3380815b0bc9feaadd60d97443daea901e5ef331
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-10-07 17:34:44 +0900

    バリテーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,8 +40,16 @@ class User < ApplicationRecord
     return user
   end
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  VALID_ZIP_REGEX = /\A\d{7}\z/
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
   validates :nick_name, presence: true, length: { maximum: 20}
-  validates :password, presence: true, confirmation: true
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
   validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
-  validates :family_name, :first_name, :family_kana, :first_kana, :birth_year, :birth_month, :birth_day, :phone_number, :zip, :prefecture_id, :city, :block, presence: true
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }, uniqueness: true
+  validates :zip, presence: true, format: { with: VALID_ZIP_REGEX }
+  validates :family_kana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :first_kana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :family_name, :first_name, :family_kana, :first_kana, :birth_year, :birth_month, :birth_day, :zip, :prefecture_id, :city, :block, presence: true
 end

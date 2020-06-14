37d15e0633558a8350535ea326d734cbf4b5869a
Yohei Higashi <yoohe0722@gmail.com>
2020-06-14 14:47:29 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 require "csv"
 
 # brand
-CSV.foreach('db/brands.csv') do |row|
-  Brand.create(name: row[0], brand_group_id: row[1])
-end
+# CSV.foreach('db/brands.csv') do |row|
+#   Brand.create(name: row[0], brand_group_id: row[1])
+# end
 
-# brand_group
-CSV.foreach('db/brand_group.csv') do |row|
-  BrandGroup.create(name: row[0])
-end
+# # brand_group
+# CSV.foreach('db/brand_group.csv') do |row|
+#   BrandGroup.create(name: row[0])
+# end
 
-# size
-CSV.foreach('db/size.csv') do |row|
-  Size.create(name: row[0])
-end
+# # size
+# CSV.foreach('db/size.csv') do |row|
+#   Size.create(name: row[0])
+# end
 
 # # user
 # user = User.new(
